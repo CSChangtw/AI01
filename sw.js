4f7933ee7bfcac4726873966aa780a740d7ae720
CSChangtw <126335519+CSChangtw@users.noreply.github.com>
2026-04-06 15:12:53 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'cpc-prompt-v1';
+const ASSETS = [
+  './', './index.html', './manifest.json',
+  './icon-72.png', './icon-96.png', './icon-128.png',
+  './icon-144.png', './icon-152.png', './icon-192.png',
+  './icon-384.png', './icon-512.png',
+  './apple-touch-icon.png', './favicon.ico'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res && res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
